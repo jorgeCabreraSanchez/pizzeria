9a64ee78fb05cef57733d656d39d6f42c158f0b1
daw <daw@daw-PC18>
2017-03-23 12:25:27 +0100

    23/03/2017 -- 12:25 Empezando pizzeria

---
+++ src/pizzeria/FXMLDocumentController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizzeria;
+
+import java.awt.Image;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ *
+ * @author daw
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private ImageView masaFina;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void masa(MouseEvent event) {
+        
+    }
+    
+}
---
+++ src/pizzeria/Pizza.java
@@ -0,0 +1,55 @@
+package pizzeria;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pizza {
+
+    private double Total;
+
+    private Map<String, Double> precioTipoMasa;
+    private String tipoMasa;
+
+    private Map<String, Double> tipoPizza;
+
+    private Map<String,Double> ingredientesExtra;
+    
+    private Map<String,Double> tamaño;
+
+   
+    public Pizza() {
+        this.precioTipoMasa = new HashMap<>();
+        this.precioTipoMasa.put("fina", 9.00);
+        this.precioTipoMasa.put("gruesa", 9.50);
+        this.tipoPizza = new HashMap<>();
+        this.tipoPizza.put("Basica", 3.00);
+        this.tipoPizza.put("CuatroQuesos", 5.00);
+        this.tipoPizza.put("Barbacoa", 7.00);
+        this.tipoPizza.put("Mexicana", 8.50);
+        this.ingredientesExtra = new HashMap<>();
+        this.ingredientesExtra.put("sinIngredientes", 0.00);
+        this.ingredientesExtra.put("jamon", 0.50);
+        this.ingredientesExtra.put("queso", 0.75);
+        this.ingredientesExtra.put("tomate", 1.50);
+        this.ingredientesExtra.put("cebolla", 2.50);
+        this.ingredientesExtra.put("olivas", 1.00);
+        this.tamaño = new HashMap<>();
+        this.tamaño.put("pequeña", 1.00);
+        this.tamaño.put("mediana", 1.15);
+        this.tamaño.put("familiar", 1.30);
+    }
+
+    public double getTotal() {
+        
+        return Total;
+    }
+
+    public void setTipoMasa(String tipo) {
+        this.precioTipoMasa.get(tipo);
+    }
+
+    
+    
+    
+    
+}
